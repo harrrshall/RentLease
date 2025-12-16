98817f6fb462d1c0fceec9d7e8e5ff640a2b96d4
harrrshall <harshalsingh.1223@gmail.com>
2025-12-16 17:15:53 +0530

    adding analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
     title: "RentLease - Tenant & Landlord Resolution Engine",
-    description: "AI-powered dispute resolution, legal insights, and practical advice for renters and property owners.",
+    description: "reddit based dispute resolution, legal insights, and practical advice for renters and property owners.",
 };
 
 export default function RootLayout({
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={inter.className} suppressHydrationWarning={true}>{children}</body>
+            <body className={inter.className} suppressHydrationWarning={true}>
+                <Analytics />
+                {children}
+            </body>
         </html>
     )
 }
